3df94d3cbb3c58920e1f14e8ad03a498e4e3349e
Timolawl <timolawl@users.noreply.github.com>
2016-01-19 09:21:15 -0800

    Add favicon cross-browser/platform support

--- js/main.js
+++ js/main.js
@@ -1,8 +1,20 @@
 /*
+ * Instead of jQuery, try Snap? Velocity is ass compared to Snap for SVG customization.
+ *
+ *
+ *
 
 $(document).ready(function() {
-    $('.rotate').velocity({
-        rotate
+    $('[id^="T-"']).on("hover", )
+    });
+});
+
+
+
+$(document).ready(function() {
+    $('.T').on("hover", function() {
+       // maybe fade out the blue and add in a real background?
+       //
     });
 });
 
