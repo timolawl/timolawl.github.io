4fa4afa08c7c87b845e3fb58200892f7444232d5
Timolawl <timolawl@users.noreply.github.com>
2016-02-09 18:19:05 -0800

    Implement responsive web elements. Hamburgers yum.

--- js/script.js
+++ js/script.js
@@ -7,11 +7,60 @@
 // main JS file
 //
 
+// hamburger behavior:
+// non-jQuery method adopted from: codepen.io/CreativeJuiz/pen/oCBxz
+// who also adopted it from http://toddmotto.com/labs/reusable-js/
+/*
+var theToggle = document.getElementById('hamburger-wrapper');
+
+// based on Todd Motto functions
+// http://toddmotto.com/labs/reusable-js/
+
+// hasClass
+function hasClass(elem, className) {
+	return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+}
+// addClass
+function addClass(elem, className) {
+    if (!hasClass(elem, className)) {
+    	elem.className += ' ' + className;
+    }
+}
+// removeClass
+function removeClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, ' ') + ' ';
+	if (hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0 ) {
+            newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    }
+}
+// toggleClass
+function toggleClass(elem, className) {
+	var newClass = ' ' + elem.className.replace( /[\t\r\n]/g, " " ) + ' ';
+    if (hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0 ) {
+            newClass = newClass.replace( " " + className + " " , " " );
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+    } else {
+        elem.className += ' ' + className;
+    }
+}
+
+theToggle.onclick = function() {
+   toggleClass(this, 'on');
+   return false;
+}
+*/
+
 // conditional loading of JS files:
 
 // make array to keep all the js file extensions to load:
 
-var scripts = [];
+// I give up...load jQuery always.
+var scripts = ["//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"];
 
 // if on mobile, load this: (not using agent sniffing because it's unreliable. best practice is to use css media query specific tags/classes specific to certain widths
 //
@@ -22,7 +71,6 @@ if (document.documentElement.clientWidth < 900) {
 
 // if at homepage, load these:
 if (window.location.pathname === '/') {
-    scripts.push("//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js");
     scripts.push("{{ '/js/vendor/date.js' | prepend: site.baseurl }}");
     scripts.push("{{ '/js/git-message.js' | prepend: site.baseurl }}");
 }
@@ -54,6 +102,12 @@ function loadEverythingElse() {
             }, false);
         }
     }
+
+    $('.hamburger-wrapper').on('click', function () {
+        $(this).toggleClass('open');
+        var openclose = $(this).hasClass('open') ? 'Close' : 'Open';
+        $(this).attr("title", openclose + ' Menu');
+    });
 
     // if at homepage:
     if (window.location.pathname === '/') {
