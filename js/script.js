98df4af492db034a25af9ded583083c45afb2945
Timolawl <timolawl@users.noreply.github.com>
2016-02-14 12:54:38 -0800

    Change js library sources to CDN. Move js snippets

--- js/script.js
+++ js/script.js
@@ -7,18 +7,18 @@
 // main JS file
 //
 
-// conditional loading of JS files:
+// conditional loading of JS files (async):
 
 // make array to keep all the js file extensions to load:
 
-// I give up...load jQuery always.
-var scripts = ["//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"];
+// Load Modernizr and jQuery
+var scripts = ["//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js", "//cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-beta1/jquery.min.js"];
 
 // if on mobile, load this: (not using agent sniffing because it's unreliable. best practice is to use css media query specific tags/classes specific to certain widths
 //
 // window.matchMedia is better because it has built in event listener for window resize. (it also matches the media to the media query of CSS.
 if (document.documentElement.clientWidth < 900) {
-    scripts.push("{{ '/js/vendor/fastclick.js' | prepend: site.baseurl }}");
+    scripts.push("//cdnjs.cloudflare.com/ajax/libs/fastclick/1.0.6/fastclick.min.js");
 }
 
 // if at homepage, load these:
@@ -38,12 +38,12 @@ function loadScripts(scripts) {
     //    console.log(script + ' loaded!');
         if (scripts.length) {
             loadScripts(scripts);
-        } else {
+        } /* else {
             loadEverythingElse();
-        }
+        } */
     };
 }
-
+/*
 function loadEverythingElse() {
     // if mobile
     if (document.documentElement.clientWidth < 900) {
@@ -54,23 +54,23 @@ function loadEverythingElse() {
         }
     }
 
-    $('.hamburger-wrapper').on('click', function () {
-        $(this).toggleClass('open');
-        var openclose = $(this).hasClass('open') ? 'Close' : 'Open';
-        $(this).attr("title", openclose + ' Menu');
-    });
+    document.getElementById('hamburger-wrapper').onclick = function() {
+        this.classList.toggle('open');
+        var openclose = this.classList.contains('open') ? 'Close' : 'Open';
+        this.setAttribute("title", openclose + ' Menu');
+    };
 
     // if at homepage:
-    if (window.location.pathname === '/') {
+
+    if(window.location.pathname === '/') {
         $(function() {
             $('.time-since-commit').loadTimeSince();
-        });
-
-        $(function() {
             $('#lastCommitMessage').loadLastCommitMessage();
         });
     }
+
 }
+*/
 
 if(scripts.length) {
     loadScripts(scripts);
